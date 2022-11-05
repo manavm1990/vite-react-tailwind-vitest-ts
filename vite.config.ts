7c00f0ed12725add1effe11cc3faa4a313875cc7
Manav Misra <manavm@visionwebsoft.com>
2022-11-05 01:53:33 -0500

    chore: :wrench: resolve aliases

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,17 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/components",
+      "@hooks": "/src/hooks",
+      "@interfaces: "/src/interfaces",
+      "@routes": "/src/routes",
+      "@services": "/src/services",
+      "@utils": "/src/utils",
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
